26eaba390da0722ac6ae9a47066831ea64bb84b0
simon cohen <escohen115@gmail.com>
2020-10-19 01:07:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,9 @@ def reverse_each_word (string)
   final = ""
   new = string.split(" ")
   new.each do |word|
-    word += " "
     final << word.reverse
+    final << " "
   end
+final.delete_suffix(" ")
 final
 end
